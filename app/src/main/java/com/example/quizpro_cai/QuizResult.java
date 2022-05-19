e0d64caf49806fe960b64fd9973fb3edcd903c42
Erick Resurreccion <resurreccionericks@gmail.com>
2022-05-20 00:10:51 +0800

    Change from firebase to SQLite. also new UI(I use instead of Button, I use radiobutton)

--- app/src/main/java/com/example/quizpro_cai/QuizResult.java
+++ app/src/main/java/com/example/quizpro_cai/QuizResult.java
@@ -2,13 +2,41 @@ package com.example.quizpro_cai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class QuizResult extends AppCompatActivity {
+
+    private TextView tvCorrect, tvIncorrect;
+    private Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_result);
+
+        tvCorrect = findViewById(R.id.tvCorrect);
+        tvIncorrect = findViewById(R.id.tvIncorrect);
+
+        btnDone = findViewById(R.id.btnDone);
+
+        final int getCorrectAns = getIntent().getIntExtra("correct",0);
+        final int getIncorrectAns = getIntent().getIntExtra("incorrect",0);
+
+
+        tvCorrect.setText("Correct Answer: " + getCorrectAns);
+        tvIncorrect.setText("Incorrect Answer: " + getIncorrectAns);
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+
     }
 }
